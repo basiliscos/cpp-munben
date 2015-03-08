dff2c052d3bff270ccc6a855190e7c3a6cd5d2ee
Ivan Baidakou <basiliscos@openmailbox.org>
2015-03-08 19:27:43 +0300

    Handle reshape, tune defaults

--- munben.cpp
+++ munben.cpp
@@ -35,6 +35,8 @@ GLuint original_texture_id;
 GLuint left_overlap_texture_id;
 GLuint right_overlap_texture_id;
 
+int width  = 1000, height = 700;
+
 void _copy_original() {
   if (left_overlap) SDL_FreeSurface(left_overlap);
   if (right_overlap) SDL_FreeSurface(right_overlap);
@@ -76,6 +78,7 @@ void _copy_original() {
     }
   }
   /* draw window borders */
+  /*
   for (int y = 0; y < 100; y++) {
     int lx = left_border;
     int rx = right_border;
@@ -83,6 +86,7 @@ void _copy_original() {
     Uint8 *rp = (Uint8 *)right_overlap->pixels + y * original->pitch + rx * bpp;
     lp[0] = lp[1] = lp[2] = rp[0] = rp[1] = rp[2] = 0;
   }
+  */
   SDL_UpdateRect(left_overlap, 0, 0, left_overlap->w, left_overlap->h);
   SDL_UpdateRect(right_overlap, 0, 0, right_overlap->w, right_overlap->h);
 }
@@ -99,10 +103,31 @@ void TW_CALL _set_overlap_callback(const void *value, void *clientData){
 
 void TW_CALL _get_overlap_callback(void *value, void *clientData) { *(float *)value = overlap; }
 
+void _reshape(int w, int h, int bpp, Uint32 flags) {
+    if( !SDL_SetVideoMode(w, h, bpp, flags) )
+    {
+        fprintf(stderr, "Video mode set failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        exit(1);
+    }
+    glViewport(0, 0, w, h);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //gluPerspective(40, (double)w/h, 1, 10);
+    //gluLookAt(0,0,3, 0,0,0, 0,1,0);
+
+    glPushMatrix();
+    glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+
+    TwWindowSize(w, h);
+}
+
 int main()
 {
     const SDL_VideoInfo* video = NULL;
-    int width  = 640, height = 480;
     int bpp, flags;
     int quit = 0;
     TwBar *bar;
@@ -130,12 +155,7 @@ int main()
     bpp = video->vfmt->BitsPerPixel;
     flags = SDL_OPENGL | SDL_HWSURFACE | SDL_RESIZABLE;
     //flags |= SDL_FULLSCREEN;
-    if( !SDL_SetVideoMode(width, height, bpp, flags) )
-    {
-        fprintf(stderr, "Video mode set failed: %s\n", SDL_GetError());
-        SDL_Quit();
-        exit(1);
-    }
+    _reshape(width, height, bpp, flags);
     SDL_WM_SetCaption("AntTweakBar simple example using SDL", "AntTweakBar+SDL");
 
     // Enable SDL unicode and key-repeat
@@ -143,7 +163,6 @@ int main()
     SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
 
     // Set OpenGL viewport and states
-    glViewport(0, 0, width, height);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0); // use default light diffuse and position
@@ -157,13 +176,14 @@ int main()
     TwInit(TW_OPENGL, NULL);
 
     // Tell the window size to AntTweakBar
-    TwWindowSize(width, height);
 
     // Create a tweak bar
     bar = TwNewBar("TweakBar");
-    float distance = -0.20f;
+    /*
+    float distance = -0.0f;
     TwAddVarRW(bar, "distance", TW_TYPE_FLOAT, &distance,
-               " max=-0.20 min=-4.0 step=0.01");
+               " max=0.0 min=-4.0 step=0.01");
+    */
     TwAddVarCB(bar, "overlap", TW_TYPE_FLOAT, _set_overlap_callback, _get_overlap_callback, NULL,
                "max=0.25 min=0.0 step=0.001 keydecr='[' keyincr=']' ");
 
@@ -188,19 +208,11 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1);
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluPerspective(40, (double)width/height, 1, 10);
-        gluLookAt(0,0,3, 0,0,0, 0,1,0);
-
-        glPushMatrix();
-        glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
 
     float texture_overlap = 2.0 * overlap;
 
-	glTranslatef( 0, 0, distance);
+    glPushMatrix();
+    //glTranslatef( 0, 0, distance);
 	glColor3d( 1, 1, 1 );
     glBindTexture(GL_TEXTURE_2D, original_texture_id);
 	glBegin(GL_QUADS); {
@@ -266,12 +278,16 @@ int main()
             // If event has not been handled by AntTweakBar, process it
             if( !handled )
             {
-                switch( event.type )
-                {
+                switch( event.type ){
                 case SDL_QUIT:  // Window is closed
-                    quit = 1;
-                    break;
-
+                  quit = 1;
+                  break;
+                case SDL_VIDEORESIZE:
+                  int w = event.resize.w;
+                  int h = event.resize.h;
+                  printf("window-event to %d x %d\n", w, h);
+                  _reshape(w, h, bpp, flags);
+                  break;
                 }
             }
         }
